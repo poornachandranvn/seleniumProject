be8ebfe719edcfbcdd594e13fabeda8fe2d79b7a
poornachandranvn <poornachandranvn@gmail.com>
2022-08-08 05:48:06 -0400

    First commit

--- src/Assignment/Assignment1.java
+++ src/Assignment/Assignment1.java
@@ -9,6 +9,8 @@ public class Assignment1 {
 		// TODO Auto-generated method stub
 	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 	WebDriver driver= new ChromeDriver();
+	
+	
 	driver.get("https://qatechhub.com/");
 	
 	driver.manage().window().maximize();
